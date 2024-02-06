7a28418219595d0d32276bab8fa3071fca7887d5
kopildas <102140363+kopildas@users.noreply.github.com>
2024-02-07 01:21:12 +0600

    feat: header design added

---
+++ app/Components/Navbar.tsx
@@ -0,0 +1,122 @@
+import Image from "next/image";
+import React from "react";
+import logo from "@/public/images/ada-logo.png";
+import Link from "next/link";
+import {
+  FaLinkedinIn,
+  FaGithub,
+  FaTwitter,
+  FaInstagram,
+  FaTiktok,
+  FaFacebookF,
+} from "react-icons/fa";
+import { TiWeatherPartlySunny } from "react-icons/ti";
+import { BsBroadcast } from "react-icons/bs";
+
+const Navbar = () => {
+  return (
+    <nav className="w-full h-40 flex bg-gradient-to-r from-slate-800 to-violet-900 px-10">
+      <div className="w-auto bg-green-00 h-full flex items-center justify-center gap-">
+        <div className="w-60 h-full bg-rd-300 flex items-center justify-center">
+          <Image src={logo} alt="ada logo" />
+        </div>
+        <div className="h-full bg-red-40 flex items-center justify-center px-5 w-auto">
+          <button className="bg-slate-300 px-2 py-2 rounded-3xl text-lg text-blue-500 w-40 flex items-center justify-center">
+          <div className="flex items-center gap-2"> <BsBroadcast className="text-2xl"/> 
+           <p className="">Live watch</p>
+           </div>
+          </button>
+        </div>
+        <div className="w-40 h-full bg-ed-200 flex items-center justify-center space-x-3">
+          <Link href="">
+            <FaLinkedinIn className="hover:text-blue-500 transition ease-in-out duration-200" />{" "}
+          </Link>
+          <Link href="">
+            <FaTiktok className="hover:text-blue-500 transition ease-in-out duration-200" />{" "}
+          </Link>
+          <Link href="">
+            <FaTwitter className="hover:text-blue-500 transition ease-in-out duration-200" />{" "}
+          </Link>
+          <Link href="">
+            <FaInstagram className="hover:text-blue-500 transition ease-in-out duration-200" />{" "}
+          </Link>
+          <Link href="">
+            <FaFacebookF className="hover:text-blue-500 transition ease-in-out duration-200" />
+          </Link>{" "}
+        </div>
+      </div>
+      <div className="w-full bg-red-90 h-full">
+        <div className="w-full h-20 bg-red-00 flex items-center justify-end gap-2">
+          <div className="text-5xl"><TiWeatherPartlySunny/></div>
+          <div className="flex flex-col">
+            <p>Today</p>
+            <p className="font-semibold text-xl flex justify-end">9 C</p>
+          </div>
+        </div>
+        <div className="w-full h-[1px] bg-gradient-to-r from-BASE_line to-slate-400"></div>
+        <div className="w-full h-[79px] bg-reen-300 flex items-center justify-end">
+          <ul>
+            <li className="space-x-10 font-semibold text-lg ">
+              <Link
+                className="hover:text-blue-500 transition ease-in-out duration-200"
+                href="/world"
+              >
+                World
+              </Link>
+              <Link
+                className="hover:text-blue-500 transition ease-in-out duration-200"
+                href="/business"
+              >
+                Business
+              </Link>
+              <Link
+                className="hover:text-blue-500 transition ease-in-out duration-200"
+                href="/science"
+              >
+                Science
+              </Link>
+              <Link
+                className="hover:text-blue-500 transition ease-in-out duration-200"
+                href="/health"
+              >
+                Health
+              </Link>
+              <Link
+                className="hover:text-blue-500 transition ease-in-out duration-200"
+                href="/sport"
+              >
+                Sport
+              </Link>
+              <Link
+                className="hover:text-blue-500 transition ease-in-out duration-200"
+                href="/lifestyle"
+              >
+                Lifestyle
+              </Link>
+              <Link
+                className="hover:text-blue-500 transition ease-in-out duration-200"
+                href="/food"
+              >
+                Food
+              </Link>
+              <Link
+                className="hover:text-blue-500 transition ease-in-out duration-200"
+                href="/travel"
+              >
+                Travel
+              </Link>
+              <Link
+                className="hover:text-blue-500 transition ease-in-out duration-200"
+                href="/bitcoin"
+              >
+                Bitcoin
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,21 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      backgroundColor: {
+        'line-1': '#2f245c',
+
+      },
+      colors: {
+        'BASE_line': '#2f245c',
+        'black': '000',
+    },
     },
   },
   plugins: [],
