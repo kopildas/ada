9ea5ea6538d6ce97735d55b4e6854e7dc7747e0f
kopildas <102140363+kopildas@users.noreply.github.com>
2024-02-07 22:37:56 +0600

    feat: api's functions

---
+++ api/index.ts
@@ -0,0 +1,8 @@
+export const getNewsTopHeadlines =async () => {
+    const newData = await fetch(`https://newsapi.org/v2/top-headlines?country=us&apiKey=${process.env.NEWS_API_TOKEN}`,{cache:"no-store"})
+    return newData.json()
+}
+export const getNewsSearch =async (keyword:string) => {
+    const newData = await fetch(`https://newsapi.org/v2/everything?apiKey=${process.env.NEWS_API_TOKEN}&q=${keyword}&pageSize=10`,{cache:"no-store"})
+    return newData.json()
+}
