ed082620df51bea157a8fba13d5836bc05165c95
kopildas <102140363+kopildas@users.noreply.github.com>
2024-02-16 19:54:38 +0600

    feat: middleware added for page view count

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import { analytics } from "./utils/analytics";
+
+export default async function middleware(req: NextRequest) {
+  if (req.nextUrl.pathname === "/" ) {
+    try {
+        analytics.track('pageview', {
+          page: '/',
+          country: req.geo?.country,
+        })
+      console.log("tracked hihihi");
+    } catch (err) {
+      // fail silently to not affect request
+      console.error(err);
+    }
+  }
+
+  return NextResponse.next();
+}
+
+export const matcher = {
+  matcher: ["/"],
+};
