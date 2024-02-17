6c69b0e54aee245ea4c0c9bfcc80a8aebe70a479
kopildas <102140363+kopildas@users.noreply.github.com>
2024-02-17 12:29:29 +0600

    feat: add authOptions to option.ts file

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,4 +1,4 @@
-import NextAuth, {NextAuthOptions} from "next-auth";
+import NextAuth from "next-auth";
 import { Account, User as AuthUser } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
 import CredentialsProvider from "next-auth/providers/credentials";
@@ -6,78 +6,9 @@ import bcrypt from "bcryptjs";
 import User from "@/models/User";
 import { connectToDatabase } from "@/utils/connectMongo";
 import { Db } from "mongodb";
+import { authOptions } from "./option";
 
-const authOptions: NextAuthOptions = {
-  // Configure one or more authentication providers
-  providers: [
-    CredentialsProvider({
-      id: "credentials",
-      name: "Credentials",
-      credentials: {
-        email: { label: "Email", type: "text" },
-        password: { label: "Password", type: "password" },
-      },
-      async authorize(credentials: any): Promise<any | null> {
-        const connection = await connectToDatabase();
-        if (!connection) {
-          // response.status(500).json({ error: 'Failed to connect to database' });
-          console.log("not connected");
-          return;
-        }
-        const { database }: { database: Db } = connection;
-        const collection = database.collection(
-          process.env.NEXT_ATLAS_COLLECTION_2 as string
-        );
-        try {
-          const user = await collection.findOne({ email: credentials.email });
-          if (user) {
-            const isPasswordCorrect = await bcrypt.compare(
-              credentials.password,
-              user.password
-            );
-            if (isPasswordCorrect) {
-              return user;
-            }
-          }
-        } catch (err: any) {
-            console.log(err)
-          throw new Error(err);
-        }
-    
-      },
-    }),
-    GithubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
-    }),
-    // ...add more providers here
-  ],
-  callbacks: {
-    async signIn({ user, account }: { user: AuthUser; account: Account }): Promise<any | null> {
-      if (account?.provider == "credentials") {
-        return true;
-      }
-      // if (account?.provider == "github") {
-      //   await connect();
-      //   try {
-      //     const existingUser = await User.findOne({ email: user.email });
-      //     if (!existingUser) {
-      //       const newUser = new User({
-      //         email: user.email,
-      //       });
 
-      //       await newUser.save();
-      //       return true;
-      //     }
-      //     return true;
-      //   } catch (err) {
-      //     console.log("Error saving user", err);
-      //     return false;
-      //   }
-      // }
-    },
-  },
-};
 
-const handler = NextAuth(authOptions);
-export { handler as GET, handler as POST, authOptions };
+export const handler = NextAuth(authOptions);
+export { handler as GET, handler as POST };
