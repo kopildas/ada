855239f213c105215c0242ba398443b3f63a77b7
kopildas <102140363+kopildas@users.noreply.github.com>
2024-02-18 17:10:05 +0600

    refactor: 🎨add try catch and send NextResponse

--- app/api/addviewer/route.ts
+++ app/api/addviewer/route.ts
@@ -8,32 +8,41 @@ import { NextResponse } from "next/server";
 export const POST = async (request: any) => {
   const { namespace, key } = await request.json();
 
-//   console.log("eloo "+key)
-//   console.log("eloo "+namespace)
+  //   console.log("eloo "+key)
+  //   console.log("eloo "+namespace)
 
-  const connection = await connectToDatabase();
-  if (!connection) {
-    // response.status(500).json({ error: 'Failed to connect to database' });
-    console.log("not connected");
-    return;
-  }
-  const { database }: { database: Db } = connection;
+  try {
+    
+    const connection = await connectToDatabase();
+    if (!connection) {
+      // response.status(500).json({ error: 'Failed to connect to database' });
+      console.log("not connected");
+      return;
+    }
+    const { database }: { database: Db } = connection;
 
-  const name= key;
+    const name = key;
 
-  const collection2 = database.collection(name as string);
-  const existingUser = await collection2.findOne({ name });
+    const collection2 = database.collection(name as string);
+    const existingUser = await collection2.findOne({ name });
 
-  if (existingUser) {
-    await collection2.updateOne(
-      { name: key },
-      { $inc: { view: 1, "metrics.orders": 1 } }
-    );
-  } else {
-    const results = await collection2.insertOne({
-      name: key,
-      view: 1,
+    if (existingUser) {
+      await collection2.updateOne(
+        { name: key },
+        { $inc: { view: 1, "metrics.orders": 1 } }
+      );
+    } else {
+      const results: any = await collection2.insertOne({
+        name: key,
+        view: 1,
+      });
+      return new NextResponse(results, {
+        status: 200,
+      });
+    }
+  } catch (err: any) {
+    return new NextResponse(err, {
+      status: 502,
     });
-    console.log(results);
   }
 };
