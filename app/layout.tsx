93777d462be87804a8128d649ac225af6839bbf2
kopildas <102140363+kopildas@users.noreply.github.com>
2024-02-22 02:02:51 +0600

    fix: trying to fix caching issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,8 @@
+export const dynamic = "force-dynamic";
+export const fetchCache = 'force-no-store'
+
+
+
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
