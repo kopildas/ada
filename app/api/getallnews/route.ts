d8e42801549f2b64d5ce4bcde0d03b4204c86fc5
kopildas <102140363+kopildas@users.noreply.github.com>
2024-02-21 19:22:49 +0600

    feat: get all news api added

---
+++ app/api/getallnews/route.ts
@@ -0,0 +1,38 @@
+import { connectToDatabase } from "@/utils/connectMongo";
+import User from "@/models/User";
+import mongoose from "mongoose";
+import { Db } from "mongodb";
+import bcrypt from "bcryptjs";
+import { NextResponse } from "next/server";
+
+export const GET = async () => {
+//   const { namespace, key } = await request.json();
+
+  try {
+    const connection = await connectToDatabase();
+    if (!connection) {
+      const errorResponse:any = { error: 'Failed to connect to database' };
+      // console.log("Error response:", JSON.stringify(errorResponse)); // Log error response
+      return new NextResponse(errorResponse, { status: 500 });
+    }
+
+    const { database }: { database: Db } = connection;
+
+    let name = "all";
+
+    const collection2 = database.collection(name as string);
+    // name = key;
+
+    let all_news: any = await collection2.find().toArray();
+    all_news = all_news.reverse();
+    
+
+    const successResponse:any = { message: "Getting all viewers", data: all_news };
+    // console.log("Success response:", JSON.stringify(successResponse)); // Log success response
+    return new NextResponse(JSON.stringify(successResponse), { status: 200 });
+  } catch (err: any) {
+    const errorResponse:any = { error: err.message };
+    // console.log("Error response:", JSON.stringify(errorResponse)); // Log error response
+    return new NextResponse(errorResponse, { status: 502 });
+  }
+};
