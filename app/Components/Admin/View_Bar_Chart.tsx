2cb6b2bcbe34edeb56cfa07520d0fe103be2cd0a
kopildas <102140363+kopildas@users.noreply.github.com>
2024-02-21 20:01:36 +0600

    fix: type error in View_Bar_Chart component props

--- app/Components/Admin/View_Bar_Chart.tsx
+++ app/Components/Admin/View_Bar_Chart.tsx
@@ -1,30 +1,28 @@
-'use client'
-
 import { BarChart } from '@tremor/react';
-import React from 'react'
+import React from 'react';
 
-export const View_Bar_Chart = ({data}) => {
-  
+interface Props {
+  data: Array<{ _id: string; name: string; view: number; metrics?: { orders: number } }>;
+}
+
+export const View_Bar_Chart: React.FC<Props> = ({ data }) => {
   const formattedData = data.map((item) => ({
     name: item.name.split("::").pop(), // Extract only the date part
     Visitors: item.view
   }));
 
-
-
   return (
     <div className="w-full h-auto bg-slate-400 mt-10 ml-10 rounded-lg bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-60 border border-gray-100 p-5 text-slate-100">
       {data && (
-       <BarChart
-       allowDecimals={false}
-       showAnimation
-       data={formattedData}
-       categories={['Visitors']}
-       index='name'
-       className='text-slate-100'
-     />
+        <BarChart
+          allowDecimals={false}
+          showAnimation
+          data={formattedData}
+          categories={['Visitors']}
+          index='name'
+          className='text-slate-100'
+        />
       )}
     </div>
   );
-  
 }
