5fa3a5b46bb7404f903013498d66c39d787e05cd
kopildas <102140363+kopildas@users.noreply.github.com>
2024-02-23 18:52:12 +0600

    feat: new type ViewerData added

--- utils/types.ts
+++ utils/types.ts
@@ -11,3 +11,23 @@ export type news= {
     publishedAt:string,
     content:string,
 }
+
+export type ViewerData ={
+    map(arg0: (item: any) => {
+      name: any; // Extract only the date part
+      Visitors: any;
+    }): any;
+    message: string;
+    data: {
+      _id: string;
+      name: string;
+      view: number;
+      metrics: {
+        orders: number;
+      };
+    }[];
+    totalViews: number;
+    todayViews: number;
+    today: string;
+    averageView: number;
+  }
