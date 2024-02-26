5173ee901d667cb096e0cdc83cc53a578de5a8c4
kopildas <102140363+kopildas@users.noreply.github.com>
2024-02-26 23:38:55 +0600

    feat: added loadder component

---
+++ app/Components/Loadder.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Loadder = () => {
+  return (
+    <div className="w-full h-screen flex items-center justify-center">
+         <div className="loadingio-spinner-rolling-hzwlgzevidp">
+              <div className="ldio-juih00wo6og">
+                <div></div>
+              </div>
+            </div>
+    </div>
+  )
+}
