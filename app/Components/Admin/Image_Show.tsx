0c7e2905cad88b91ebb8fcd0416e42867037444c
kopildas <102140363+kopildas@users.noreply.github.com>
2024-02-28 22:09:24 +0600

    fix: image fitting issue on add news page

--- app/Components/Admin/Image_Show.tsx
+++ app/Components/Admin/Image_Show.tsx
@@ -1,20 +1,22 @@
-import React from 'react'
 import Image from "next/image";
+import React from "react";
 interface Props {
-    imageURL: string;
-  }
-export const Image_Show: React.FC<Props> = ({imageURL}) => {
-  console.log(imageURL)
-    return (
-    <div className="relative w-80 h-96 rounded-xl ">
-        {imageURL && (
-          <Image
-            src={imageURL}
-            alt="tt"
-            fill
-            className="rounded-xl"
-          />
-        )}
-      </div>
-  )
+  imageURL: string;
 }
+export const Image_Show: React.FC<Props> = ({ imageURL }) => {
+  console.log(imageURL);
+  return (
+    <div className="relative w-80 h-96 rounded-xl">
+      {imageURL && (
+        <Image
+          src={imageURL}
+          alt="tt"
+          fill
+          // layout="responsive" // Ensure responsive behavior
+         
+          className="rounded-xl w-80 object-cover"
+        />
+      )}
+    </div>
+  );
+};
