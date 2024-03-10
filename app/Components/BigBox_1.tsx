62b708f68dc6e72cbba52a902a22b3736c0a9997
kopildas <102140363+kopildas@users.noreply.github.com>
2024-03-10 18:36:43 +0600

    fix: responsiveness and added Link

--- app/Components/BigBox_1.tsx
+++ app/Components/BigBox_1.tsx
@@ -1,11 +1,13 @@
 import { news } from "@/utils/types";
 import Image from "next/image";
+import Link from "next/link";
 
 const BigBox_1 = ({ data }: { data: news }) => {
   const truncatedTitle = data.title.length > 60 ? data.title.slice(0, 60) + '...' : data.title;
 
   return (
-    <div className="w-1/3 flex flex-col">
+    <Link
+    href={`/${data.category}/${data._id}`} className="w-1/3 flex flex-col">
       <div className="relative aspect-video rounded-xl ">
         {data.urlToImage && (
           <Image
@@ -21,7 +23,7 @@ const BigBox_1 = ({ data }: { data: news }) => {
         <p className="text-3xl font-bold text-zinc-800  mb-2">{truncatedTitle}</p>
         <p className="text-xl mb-4 text-zinc-500">{data.author}</p>
       </div>
-    </div>
+    </Link>
   );
 };
 
--- app/Components/BigBox_2.tsx
+++ app/Components/BigBox_2.tsx
@@ -4,12 +4,13 @@ import React from 'react'
 
 export const BigBox_2 = ({ data }: { data: news }) => {
   // Truncate the title if it exceeds 40 characters
-  const truncatedTitle = data.title.length > 40 ? data.title.slice(0, 30) + '...' : data.title;
+  const truncatedTitle = data.title.length > 30 ? data.title.slice(0, 10) + '...' : data.title;
+  const truncatedTitle2 = data.title.length > 30 ? data.title.slice(0, 17) + '...' : data.title;
 // console.log(truncatedTitle)
   return (
-    <div className='flex flex-col w-full mb-10 bg-blue-0 items-center justify-center'>
-        <div className='md:w-2/5 bg-red-00 flex items-center justify-center bg-green-100'>
-            <div className="relative aspect-video h-[220px] w-[350px] rounded-xl ">
+    <div className='flex flex-col w-full mb-10 bg-blue-00 items-center justify-center'>
+        <div className='md:w-[140px] lg:w-[300px] w-full bg-red-00 flex items-center justify-center bg-green-00'>
+            <div className="relative aspect-video w-[150px] h-[100px] md:h-[120px] bg-green-00 lg:h-[220px] md:w-[140px] lg:w-[300px] rounded-xl ">
                 {data.urlToImage && (
                   <Image
                     src={data.urlToImage}
@@ -20,10 +21,11 @@ export const BigBox_2 = ({ data }: { data: news }) => {
                 )}
             </div>
         </div>
-        <div className='md:w-3/5 flex flex-col items-start justify-center ml-7 md:-ml-14 bg-red-00 text-zinc-900'>
+        <div className='md:w-[140px] lg:w-[300px] flex flex-col items-start md:justify-center mr-5 md:mr-0 bg-red-00 text-zinc-900'>
               
-              {truncatedTitle && <p className="text-2xl font-bold mt-4 mb-2">{truncatedTitle} <span className="text-sm text-blue-500"></span></p>}
-              <p className="text-xl mb-4 text-zinc-500">{data.author}</p>
+              {truncatedTitle && <p className="block lg:hidden lg:text-2xl text-lg font-bold md:mt-4 mb-2">{truncatedTitle}</p>}
+              {truncatedTitle2 && <p className="lg:block hidden lg:text-2xl text-lg font-bold md:mt-4 mb-2">{truncatedTitle2}</p>}
+              <p className="lg:text-xl text-xs mb-4 text-zinc-500">{data.author} y</p>
         </div>
     </div>
   )
