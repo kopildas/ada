0bff98f7c62f5e3e9f5e18494bb3890c4e36cb18
kopildas <102140363+kopildas@users.noreply.github.com>
2024-03-10 18:30:07 +0600

    fix: responsive on mobile and tablet

--- app/Components/TextMarquee.tsx
+++ app/Components/TextMarquee.tsx
@@ -4,7 +4,7 @@ import React from "react";
 const TextMarquee = ({ data }: { data: news[] }) => {
   // console.log(data);
   return (
-    <div className="relative flex text-slate-200 overflow-x-hidden bg-gradient-to-r from-slate-800 to-violet-900 rounded-[3.5rem] md:mt-[55rem] lg:mt-[15rem] bg-green-300">
+    <div className="relative flex text-slate-200 overflow-x-hidden bg-gradient-to-r from-slate-800 to-violet-900 rounded-[3.5rem] mt-[58rem] md:-mt-[10rem] lg:mt-[15rem] bg-green-300">
       <div className=" flex items-center justify-center text-2xl bg-slate-800 z-20">
         <p className="bg-slate-200 text-zinc-600 px-5 py-[6px] rounded-[3.5rem] font-bold ml-2">
           {" "}
