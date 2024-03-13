ae36b3a16bd3cb3ac0e208065248cf409a04cd75
kopildas <102140363+kopildas@users.noreply.github.com>
2024-03-13 21:52:42 +0600

    feat: related box design 1 added

---
+++ app/Components/CategoryPage/Related_Big_box_1.tsx
@@ -0,0 +1,39 @@
+'use client'
+
+
+import { news } from '@/utils/types'
+import Image from 'next/image'
+import React from 'react'
+import Link from "next/link";
+import { useToggleState } from '@/state/toggle_menu';
+
+
+export const Related_Big_box_1 = ({ data }: { data: news }) => {
+  const {isOpen} = useToggleState()
+
+  const truncatedTitle = data.title.length > 30 ? data.title.slice(0, 15) + '...' : data.title;
+  const truncatedTitle2 = data.title.length > 30 ? data.title.slice(0, 35) + '...' : data.title;
+// console.log(truncatedTitle)
+  return (
+    <Link  href={`/${data.category}/${data._id}`} className='flex flex-col w-full lg:mb-10 bg-blue-00 lg:items-center lg:justify-center h-fit'>
+        <div className=' w-full bg-red-00 flex items-center justify-center bg-green-00 md:px-5'>
+            <div className="relative w-full lg:h-80 aspect-video   rounded-xl bg-cover overflow-hidden">
+                {data.urlToImage && (
+                  <Image
+                    src={data.urlToImage}
+                    alt={data.title}
+                    className={`rounded-xl hover:scale-110 duration-500 transition ease-in-out ${isOpen ? "-z-10" : "z-10"}`}
+                    fill
+                  />
+                )}
+            </div>
+        </div>
+        <div className=' w-full flex flex-col items-start md:justify-center  md:ml-0 md:mr-0 bg-red-00 text-zinc-900 md:px-5'>
+              
+              {truncatedTitle && <p className="block lg:hidden lg:text-xl text-lg font-bold md:mt-2">{truncatedTitle}</p>}
+              {truncatedTitle2 && <p className="lg:block hidden lg:text-xl text-lg font-bold md:mt-2">{truncatedTitle2}</p>}
+              <p className="lg:text-xl text-xs md:mb-4 lg:mb-4 text-zinc-500">{data.author} y</p>
+        </div>
+    </Link>
+  )
+}
---
+++ app/Components/CategoryPage/Related_News.tsx
@@ -0,0 +1,35 @@
+// import { getRUSNews } from "@/api";
+// import { removeDuplicate } from "@/utils";
+// import BigBox_1 from "./BigBox_1";
+// import { BigBox_2 } from "./BigBox_2";
+// import { getNewsByCategory, getRUSNews } from "../api";
+import { getNewsByCategory } from "@/app/api";
+import Link from "next/link";
+import { Related_Big_box_1 } from "./Related_Big_box_1";
+
+
+export const Related_News = async ({category}:{category:string}) => {
+  const fetchingData = async () => {
+    const newsTop = await getNewsByCategory(category);
+    // const filterData = removeDuplicate(newsTop);
+    // console.log(newsTop);
+//     console.log(filterData);
+
+    return newsTop;
+  };
+
+  const data = await fetchingData();
+  const singledata = data[3];
+  // console.log(data);
+  const sliceData = data.slice(0, 6);
+  return (
+    <div className="w-full flex flex-wrap items-center justify-center bg-green-00 gap-5 md:gap-0">
+      {sliceData &&
+        sliceData.map((item, indx) => (
+          <div key={indx}className="md:w-1/3 flex gap-5">
+            <Related_Big_box_1 data={item} />
+          </div>
+        ))}
+    </div>
+  );
+};
