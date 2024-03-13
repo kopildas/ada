6bbef3fa514349a154e1c884535d288539faa013
kopildas <102140363+kopildas@users.noreply.github.com>
2024-03-13 21:19:32 +0600

    feat: added zoom effect and truncated title

--- app/Components/SmallBox_2.tsx
+++ app/Components/SmallBox_2.tsx
@@ -1,27 +1,35 @@
+'use client'
+
+
 import { news } from '@/utils/types'
 import Image from 'next/image'
 import React from 'react'
 import Link from "next/link";
+import { useToggleState } from '@/state/toggle_menu';
 
 export const SmallBox_2 = ({ data }: { data: news }) => {
+  const truncatedTitle = data.title.length > 30 ? data.title.slice(0, 30) + '...' : data.title;
+  const truncatedTitle2 = data.title.length > 30 ? data.title.slice(0, 37) + '...' : data.title;
+  const {isOpen} = useToggleState()
   return (
     <Link
           href={`/${data.category}/${data._id}`} className='flex flex-col bg-red-00 lg:flex-row w-full mb-10 gap-2'>
         <div className=' md:w-[300px] bg-red-00 flex items-center justify-center'>
-        <div className="relative aspect-video h-[220px] md:w-[300px] rounded-xl ">
+        <div className="relative aspect-video h-[220px] md:w-[300px] rounded-xl bg-cover overflow-hidden">
                 {data.urlToImage && (
                   <Image
                     src={data.urlToImage}
                     alt={data.title}
+                    className={`rounded-xl hover:scale-110 duration-500 transition ease-in-out ${isOpen ? "-z-10" : "z-10"}`}
                     fill
-                    className="rounded-xl"
                   />
                 )}
               </div>
         </div>
         <div className='lg:w-3/5 flex flex-col items-start justify-center lg:mr-5 bg-red-00 text-zinc-900'>
               {/* <p className="text-blue-500 mt-2 md:mt-0">{data.source.name}</p> */}
-              <p className="text-xl font-bold md:mt-4 md:mb-6">{data.title}</p>
+              {truncatedTitle && <p className="block lg:hidden lg:text-xl text-lg font-bold md:mt-4 mb-2">{truncatedTitle}</p>}
+              {truncatedTitle2 && <p className="lg:block hidden lg:text-xl text-lg font-bold md:mt-4 mb-2">{truncatedTitle2}</p>}
               <p className="text-xl md:mb-4 text-zinc-500">{data.author}</p>
               </div>
     </Link>
