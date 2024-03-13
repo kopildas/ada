e7c4830bc7465ea7b5557a3c4df2a71ed396ee5b
kopildas <102140363+kopildas@users.noreply.github.com>
2024-03-13 21:50:13 +0600

    feat: choice box component added

---
+++ app/Components/CategoryPage/Choice_Box.tsx
@@ -0,0 +1,36 @@
+// import { getRUSNews } from "@/api";
+// import { removeDuplicate } from "@/utils";
+// import BigBox_1 from "./BigBox_1";
+// import { BigBox_2 } from "./BigBox_2";
+// import { getNewsByCategory, getRUSNews } from "../api";
+import { getNewsByCategory } from "@/app/api";
+import Link from "next/link";
+import { Related_Big_box_1 } from "./Related_Big_box_1";
+import { Choi_Box_1 } from "./Choi_Box_1";
+
+
+export const Choice_Box = async ({category}:{category:string}) => {
+  const fetchingData = async () => {
+    const newsTop = await getNewsByCategory(category);
+    // const filterData = removeDuplicate(newsTop);
+    // console.log(newsTop);
+//     console.log(filterData);
+
+    return newsTop;
+  };
+
+  const data = await fetchingData();
+  const singledata = data[3];
+  // console.log(data);
+  const sliceData = data.slice(0, 6);
+  return (
+    <div className="w-full flex flex-col items-center justify-center bg-green-00 gap-5 md:gap-0">
+      {sliceData &&
+        sliceData.map((item, indx) => (
+          <div key={indx}className="md:w-full flex gap-5">
+            <Choi_Box_1 data={item} />
+          </div>
+        ))}
+    </div>
+  );
+};
