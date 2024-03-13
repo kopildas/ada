b828b467341a77962be7be103c460ddee90806fc
kopildas <102140363+kopildas@users.noreply.github.com>
2024-03-13 21:27:47 +0600

    fix: sliced data and responsiveness fixed

--- app/Components/TextMarquee.tsx
+++ app/Components/TextMarquee.tsx
@@ -3,8 +3,9 @@ import React from "react";
 
 const TextMarquee = ({ data }: { data: news[] }) => {
   // console.log(data);
+  const sliceData = data.slice(0,6)
   return (
-    <div className="relative flex text-slate-200 overflow-x-hidden bg-gradient-to-r from-slate-800 to-violet-900 rounded-[3.5rem] mt-[58rem] md:-mt-[10rem] lg:mt-[10rem] bg-green-300">
+    <div className="relative flex text-slate-200 overflow-x-hidden bg-gradient-to-r from-slate-800 to-violet-900 rounded-[3.5rem] mt-[58rem] md:-mt-[25rem] lg:mt-[10rem] bg-green-300">
       <div className=" flex items-center justify-center text-2xl bg-slate-800 z-20">
         <p className="bg-slate-200 text-zinc-600 px-5 py-[6px] rounded-[3.5rem] font-bold ml-2">
           {" "}
@@ -12,8 +13,8 @@ const TextMarquee = ({ data }: { data: news[] }) => {
         </p>
       </div>
       <div className="py-3 animate-marquee whitespace-nowrap z-10 -mx-24">
-        {data &&
-          data.map((item, ind) => (
+        {sliceData &&
+          sliceData.map((item, ind) => (
             <span key={ind} className="mx-4 text-2xl">
               {item.title}
             </span>
@@ -22,8 +23,8 @@ const TextMarquee = ({ data }: { data: news[] }) => {
       </div>
 
       <div className="absolute top-0 py-3 animate-marquee2 whitespace-nowrap">
-        {data &&
-          data.map((item, ind) => (
+        {sliceData &&
+          sliceData.map((item, ind) => (
             <span key={ind} className="mx-4 text-2xl">
               {item.title}
             </span>
