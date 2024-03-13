a85385f3d698a39a7f5efc61cbd7880752241346
kopildas <102140363+kopildas@users.noreply.github.com>
2024-03-13 20:42:34 +0600

    feat: zoom image and truncate title

--- app/Components/SmallBox_1.tsx
+++ app/Components/SmallBox_1.tsx
@@ -1,12 +1,17 @@
+'use client'
+
+
 import { news } from "@/utils/types";
 import Image from "next/image";
 import React from "react";
 import Link from "next/link";
+import { useToggleState } from "@/state/toggle_menu";
 
 
 function SmallBox_1({ data }: { data: news }) {
-  const truncatedTitle = data.title.length > 40 ? data.title.slice(0, 30) + '...' : data.title;
-
+   const {isOpen} = useToggleState()
+  const truncatedTitle = data?.title.length > 10 ? data.title.slice(0, 10) + '...' : data.title;
+  const truncatedTitle2 = data?.title.length > 10 ? data.title.slice(0, 20) + '...' : data.title;
   // Extracting date portion from publishedAt timestamp
   const publishedDate = data.publishedAt.slice(0, 10); // Extracts characters from index 0 to 9
 
@@ -14,20 +19,20 @@ function SmallBox_1({ data }: { data: news }) {
     <Link
     href={`/${data.category}/${data._id}`} className="w-full flex flex-col lg:flex-row p-5 md:ml-6 lg:ml-0 -mt-5 lg:mt-0">
       <div className="lg:w-2/5">
-        <div className="relative aspect-video rounded-xl ">
-          {data.urlToImage && (
-            <Image
-              src={data.urlToImage}
-              alt={data.title}
+        <div className="relative aspect-video rounded-xl bg-cover overflow-hidden">
+                {data.urlToImage && (
+                  <Image
+                    src={data.urlToImage}
+                    alt={data.title}
+                    className={`rounded-xl hover:scale-110 duration-500 transition ease-in-out ${isOpen ? "-z-10" : "z-10"}`}
               fill
-              className="rounded-xl"
             />
           )}
         </div>
       </div>
       <div className="lg:w-3/5 lg:ml-5">
-        <p className="text-lg lg:text-xl font-bold text-zinc-800  md:mb-2">{truncatedTitle}</p>
-        <p className="text-base text-zinc-500">{publishedDate}</p>
+      {truncatedTitle && <p className="block lg:hidden md:text-xl text-base font-bold md:mt- mb- text-gray-800">{truncatedTitle}</p>}
+      {truncatedTitle2 && <p className="lg:block hidden lg:text-xl text-base font-bold md:mt- mb-2 text-gray-800">{truncatedTitle2}</p>}         <p className="text-base text-zinc-500">{publishedDate}</p>
       </div>
     </Link>
   );
