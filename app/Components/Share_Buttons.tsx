91cf5b62cc5b3b0e491275b224e5fc35cb949bc0
kopildas <102140363+kopildas@users.noreply.github.com>
2024-03-15 15:05:21 +0600

    feat: social share btns added on single news page

---
+++ app/Components/Share_Buttons.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+import React from "react";
+import {
+  EmailShareButton,
+  FacebookShareButton,
+  WhatsappShareButton,
+  TwitterShareButton,
+  TelegramShareButton,
+  EmailIcon,
+  FacebookIcon,
+  WhatsappIcon,
+  TwitterIcon,
+  TelegramIcon,
+} from "react-share";
+
+function Share_Buttons() {
+  const currentURL = usePathname();
+  const link = `${process.env.NEXTAUTH_URL}/${currentURL}`;
+  console.log(link);
+  console.log(currentURL);
+
+  return (
+    <div className="flex gap-3 bg-red-0">
+      <EmailShareButton url={link}>
+          <EmailIcon className="w-8 h-8 rounded-full hover:scale-110 duration-500 transition ease-in-out "/>
+      </EmailShareButton>
+      <FacebookShareButton url={link}>
+          <FacebookIcon className="w-8 h-8 rounded-full hover:scale-110 duration-500 transition ease-in-out "/>
+      </FacebookShareButton>
+      <WhatsappShareButton url={link}>
+          <WhatsappIcon className="w-8 h-8 rounded-full hover:scale-110 duration-500 transition ease-in-out "/>
+      </WhatsappShareButton>
+      <TwitterShareButton url={link}>
+          <TwitterIcon className="w-8 h-8 rounded-full hover:scale-110 duration-500 transition ease-in-out "/>
+      </TwitterShareButton>
+      <TelegramShareButton url={link}>
+          <TelegramIcon className="w-8 h-8 rounded-full hover:scale-110 duration-500 transition ease-in-out "/>
+      </TelegramShareButton>
+    </div>
+  );
+}
+
+export default Share_Buttons;
