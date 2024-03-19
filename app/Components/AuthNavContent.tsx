11b775c163e595d74513c91364a8f4ccdb6d1419
kopildas <102140363+kopildas@users.noreply.github.com>
2024-03-20 02:11:02 +0600

    fix: flex directions

--- app/Components/AuthNavContent.tsx
+++ app/Components/AuthNavContent.tsx
@@ -1,41 +1,50 @@
 "use client";
 import Link from "next/link";
 import React from "react";
-import {signOut, useSession} from "next-auth/react"
+import { signOut, useSession } from "next-auth/react";
+import { Search_Button } from "./Search_Option/Search_Button";
+import { news } from "@/utils/types";
 
-const AuthNavContent = () => {
-const {data:session}:any = useSession()
+const AuthNavContent = ({ data }: { data: news[] }) => {
+  const { data: session }: any = useSession();
   return (
     <>
-    {!session ? (
+      {!session ? (
         <>
-         <Link
-        className="hover:text-blue-500 transition ease-in-out duration-200"
-        href="/login"
-      >
-        Login
-      </Link>
-
-      <Link
-        className="hover:text-blue-500 transition ease-in-out duration-200"
-        href="/register"
-      >
-        Register
-      </Link>
+          <div className=" w-fit flex flex-col lg:flex-row  items-center justify-center bg-fuchsia-00 gap-4">
+          <Link
+            className="hover:text-blue-500 transition ease-in-out duration-200"
+            href="/login"
+          >
+            Login
+          </Link>
+          
+          <Link
+            className="hover:text-blue-500 transition ease-in-out duration-200"
+            href="/register"
+          >
+            Register
+          </Link>
+         
+          </div>
+          
         </>
-    ) : (
+      ) : (
         <>
-        {session.user?.email === "k@gmail.com" && 
-        <Link
-        className="hover:text-blue-500 transition ease-in-out duration-200"
-        href="/admin"
-      >
-        Admin
-      </Link>}
-        <button onClick={()=>signOut()}>Logout</button>
+          <div className=" w-fit flex flex-col lg:flex-row items-center justify-center bg-fuchsia-00 gap-4">
+          {session.user?.email === "k@gmail.com" && (
+            <Link
+              className="hover:text-blue-500 transition ease-in-out duration-200"
+              href="/admin"
+            >
+              Admin
+            </Link>
+          )}
+          <button onClick={() => signOut()}>Logout</button>
+          
+          </div>
         </>
-    ) }
-     
+      )}
     </>
   );
 };
