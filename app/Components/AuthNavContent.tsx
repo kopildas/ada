e2206fde3cf05a62543364d2b203cc372eb7653d
kopildas <102140363+kopildas@users.noreply.github.com>
2024-03-20 05:02:41 +0600

    fix: search button visibility in mobile screen

--- app/Components/AuthNavContent.tsx
+++ app/Components/AuthNavContent.tsx
@@ -12,36 +12,39 @@ const AuthNavContent = () => {
       {!session ? (
         <>
           <div className=" w-fit flex flex-col lg:flex-row  items-center justify-center bg-fuchsia-00 gap-4">
-          <Link
-            className="hover:text-blue-500 transition ease-in-out duration-200"
-            href="/login"
-          >
-            Login
-          </Link>
-          
-          <Link
-            className="hover:text-blue-500 transition ease-in-out duration-200"
-            href="/register"
-          >
-            Register
-          </Link>
-         
+            <Link
+              className="hover:text-blue-500 transition ease-in-out duration-200"
+              href="/login"
+            >
+              Login
+            </Link>
+
+            <Link
+              className="hover:text-blue-500 transition ease-in-out duration-200"
+              href="/register"
+            >
+              Register
+            </Link>
+            <div className="hidden lg:block">
+              <Search_Button />
+            </div>
           </div>
-          
         </>
       ) : (
         <>
           <div className=" w-fit flex flex-col lg:flex-row items-center justify-center bg-fuchsia-00 gap-4">
-          {session.user?.email === "k@gmail.com" && (
-            <Link
-              className="hover:text-blue-500 transition ease-in-out duration-200"
-              href="/admin"
-            >
-              Admin
-            </Link>
-          )}
-          <button onClick={() => signOut()}>Logout</button>
-          
+            {session.user?.email === "k@gmail.com" && (
+              <Link
+                className="hover:text-blue-500 transition ease-in-out duration-200"
+                href="/admin"
+              >
+                Admin
+              </Link>
+            )}
+            <button onClick={() => signOut()}>Logout</button>
+            <div className="hidden lg:block">
+              <Search_Button />
+            </div>
           </div>
         </>
       )}
