4db90837088cbeae35f222dd00158e78c7010ab4
kopildas <102140363+kopildas@users.noreply.github.com>
2024-03-20 02:14:13 +0600

    feat: called data from global context

--- state/toggle_menu.ts
+++ state/toggle_menu.ts
@@ -1,11 +1,24 @@
+import { news } from '@/utils/types'
 import {create} from 'zustand'
 
 interface ToggleState {
     isOpen: boolean
     setIsOpen: (isOpen: boolean) => void
+    
+    is_Search_Open: boolean
+    setIs_Search_Open: (is_Search_Open: boolean) => void
+
+    newsData: news[]
+  setNewsData: (newsData: news[]) => void; 
 }
 
 export const useToggleState = create<ToggleState>()((set)=> ({
     isOpen:false,
-    setIsOpen: (isOpen: boolean) => set({isOpen})
+    setIsOpen: (isOpen: boolean) => set({isOpen}),
+
+    is_Search_Open:false,
+    setIs_Search_Open: (is_Search_Open: boolean) => set({is_Search_Open}),
+
+    newsData: [] as news[],
+    setNewsData: (newsData: news[]) => set({ newsData }),
 }))
